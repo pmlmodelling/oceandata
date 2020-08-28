7dad8c3a2522790616d73ba67ea4d2027d9804ae
Robert Wilson <robertwilson190@gmail.com>
2020-08-28 17:21:17 +0100

    add open_sst

---
+++ oceandata/open_sst.py
@@ -0,0 +1,36 @@
+
+
+
+
+def get_sst(source = "cobe2"):
+   """
+   Search for World Ocean Atlas 2018 data
+
+   Parameters
+   -------------
+   source: str
+       source of sst data. Options are "cobe1" and "cobe2". Defaults to "cobe2"
+
+   cobe1: https://www.psl.noaa.gov/data/gridded/data.cobe.html
+   cobe2: https://www.psl.noaa.gov/data/gridded/data.cobe.html
+
+   Returns:
+   list : a list of available thredds files.
+
+   -------------
+
+   """
+
+
+   if source == "cobe1":
+       return "https://psl.noaa.gov/data/gridded/data.cobe.html"
+
+   if source == "cobe2":
+       return "https://psl.noaa.gov/thredds/dodsC/Datasets/COBE2/sst.mon.mean.nc"
+
+
+
+
+
+
+
