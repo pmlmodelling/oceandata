ad34c979081ad2f3108820dc5d7f4eef8139eb08
Robert  Wilson <robertwilson190@gmail.com>
2023-01-11 15:52:34 +0000

    add ability to find rrs

--- oceandata/open_chl.py
+++ oceandata/open_chl.py
@@ -42,7 +42,7 @@ def get_occci(var = None, res = "daily", years = None, months = range(1, 13), ve
     if type(var) is not str:
         raise ValueError("Please supply a string for var!")
 
-    if var not in ["chl", "kd"]:
+    if var not in ["chl", "kd", "rrs"]:
         raise ValueError(f"{var} is not a valid var")
 
     if res not in ["daily", "monthly", "8day"]:
@@ -93,11 +93,69 @@ def get_occci(var = None, res = "daily", years = None, months = range(1, 13), ve
                 part1 = "1M"
             if res == "8day":
                 part1 = "8D"
-            if res == "8day":
-                part2 = "DAILY"
             else:
                 part2 = res.upper()
             url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v{version}-release/geographic/{res}/chlor_a/{year}/ESACCI-OC-L3S-CHLOR_A-MERGED-{part1}_{part2}_4km_GEO_PML_OCx-{cc}-fv{version}.nc"
+            files.append(url)
+
+        return(files)
+
+    if var == "rrs":
+        ensemble = []
+        for yy in years:
+            url = f'https://rsg.pml.ac.uk/thredds/catalog/cci/v4.2-release/geographic/{res}/rrs/{yy}/catalog.html'
+            page = requests.get(url).text
+            soup = BeautifulSoup(page, 'html.parser')
+            ensemble+=[url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+        ensemble = [ff for ff in ensemble if int(ff.split("/")[-1].split("_")[-1].split("-")[1][4:6]) in months]
+        chunk = [ff.split("/")[-1].split("_")[-1].split("-")[1] for ff in ensemble]
+
+        files = []
+
+        for cc in chunk:
+            year = int(cc[0:4])
+            if res == "daily":
+                part1 = "1D"
+            if res == "monthly":
+                part1 = "1M"
+            if res == "8day":
+                part1 = "8D"
+            else:
+                part2 = res.upper()
+            #url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v4.2-release/geographic/{res}/kd/{year}/ESACCI-OC-L3S-CHLOR_A-MERGED-{part1}_{part2}_4km_GEO_PML_OCx-{cc}-fv4.2.nc"
+            url = f"https://www.oceancolour.org/thredds/dodsC/cci/v{version}-release/geographic/{res}/rrs/{year}/ESACCI-OC-L3S-RRS-MERGED-{part1}_{part2}_4km_GEO_PML_RRS-{cc}-fv{version}.nc"
+
+            files.append(url)
+
+        return(files)
+
+    if var == "kd":
+        ensemble = []
+        for yy in years:
+            url = f'https://rsg.pml.ac.uk/thredds/catalog/cci/v4.2-release/geographic/{res}/kd/{yy}/catalog.html'
+            page = requests.get(url).text
+            soup = BeautifulSoup(page, 'html.parser')
+            ensemble+=[url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+        ensemble = [ff for ff in ensemble if int(ff.split("/")[-1].split("_")[-1].split("-")[1][4:6]) in months]
+        chunk = [ff.split("/")[-1].split("_")[-1].split("-")[1] for ff in ensemble]
+
+        files = []
+
+        for cc in chunk:
+            year = int(cc[0:4])
+            if res == "daily":
+                part1 = "1D"
+            if res == "monthly":
+                part1 = "1M"
+            if res == "8day":
+                part1 = "8D"
+            else:
+                part2 = res.upper()
+            #url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v4.2-release/geographic/{res}/kd/{year}/ESACCI-OC-L3S-CHLOR_A-MERGED-{part1}_{part2}_4km_GEO_PML_OCx-{cc}-fv4.2.nc"
+            url = f"https://www.oceancolour.org/thredds/dodsC/cci/v{version}-release/geographic/{res}/kd/{year}/ESACCI-OC-L3S-K_490-MERGED-{part1}_{part2}_4km_GEO_PML_KD490_Lee-{cc}-fv{version}.nc"
+
             files.append(url)
 
         return(files)
@@ -128,7 +186,7 @@ def get_occci(var = None, res = "daily", years = None, months = range(1, 13), ve
             else:
                 part2 = res.upper()
             #url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v4.2-release/geographic/{res}/kd/{year}/ESACCI-OC-L3S-CHLOR_A-MERGED-{part1}_{part2}_4km_GEO_PML_OCx-{cc}-fv4.2.nc"
-            url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v4.2-release/geographic/daily/kd/{year}/ESACCI-OC-L3S-K_490-MERGED-{part1}_{part2}_4km_GEO_PML_KD490_Lee-{cc}-fv4.2.nc"
+            url = f"https://rsg.pml.ac.uk/thredds/dodsC/cci/v{version}-release/geographic/daily/kd/{year}/ESACCI-OC-L3S-K_490-MERGED-{part1}_{part2}_4km_GEO_PML_KD490_Lee-{cc}-fv{version}.nc"
             files.append(url)
 
         return(files)
